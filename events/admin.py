a47b0ec459b1cd0905ebed2caf71399fc186fe12
galnaaman <85794518+galnaaman@users.noreply.github.com>
2024-02-01 14:20:38 +0200

    update admin.py

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.site_header = "Events AlfaBet Admin"
+admin.site.site_title = "Events AlfaBet Admin "
+admin.site.index_title = "Welcome to Events AlfaBet Admin Portal"
+admin.site.site_url = "/swagger"
+
+admin.site.register(Event)
+
--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,6 @@
-
 from django.urls import path
-
+from .views import *
 
 urlpatterns = [
-
+    # path("v1/event/", EventView.as_view(), name="event"),
 ]
