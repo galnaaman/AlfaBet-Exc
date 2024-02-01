04840ab81bcd0ae5b04fa2a39499a570bf6fbbf0
galnaaman <85794518+galnaaman@users.noreply.github.com>
2024-02-01 13:58:50 +0200

    Create Event Model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Event(models.Model):
+    """
+    Event model - represents a single event
+    """
+    id = models.AutoField(primary_key=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+
+    # Location and venue can be usesd as geo fields need to change it later
+    location = models.CharField(max_length=255)
+    venue = models.CharField(max_length=255)
+
+    participants = models.ManyToManyField(User, related_name='participating_events', blank=True)
+
+    start_time = models.DateTimeField(blank=False, null=False)
+    end_time = models.DateTimeField(blank=False, null=False)
+    creation_time = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def popularity(self):
+        return self.participants.count()
+
+    @property
+    def is_past(self):
+        return self.end_time < timezone.now()
+
+    class Meta:
+        db_table = "events"
+        ordering = ['id']
+
